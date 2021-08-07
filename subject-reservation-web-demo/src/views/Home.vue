67d094ce286583e323957eb5e0047dbc824b7324
Nattima Mahapan <nattima.09@mail.kmutt.ac.th>
2021-08-07 11:57:40 +0700

    update title

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by Nattima (test)
+      SIT Subject Reservation Web by Nattima0912
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
